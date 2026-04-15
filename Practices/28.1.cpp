997ccf4f330b045ab83afa85900dc523df811033
Florist Cafe <floristcafe2.4@gmail.com>
2026-04-15 17:51:32 +0800

    Create 28.1.cpp

---
+++ Practices/28.1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+  int n,i;
+double sum;
+cin>>sum;
+double a;
+for(int i=0;i<n;i++){
+  sum/=(1+i);
+cin>>a;
+   sum+=a;
+}
+cout<<sum<<endl;
+return 0;
+}
